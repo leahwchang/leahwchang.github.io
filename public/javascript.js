3d45cfd13b92d72a4ff82aa5f4a3be710d44bdf9
Leah W. Chang <leahwchang@gmail.com>
2016-12-01 12:01:16 -0500

    add modified smooth scrolling, bootstrap scrollspy, anchor tag positions on another page

--- public/javascript.js
+++ public/javascript.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   // css tricks
   // add scrollspy to <body>
-  $('body').scrollspy({target: "#navi", offset: 50});
+  $('body').scrollspy({target: '#navbar'});
   // add smooth scrolling on all links inside the navbar
-  $("#navi a").on('click', function(event) {
+  $("#navbar a").on('click', function(event) {
     // make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // prevent default anchor click behavior
@@ -11,36 +11,44 @@ $(document).ready(function(){
       // store hash
       var hash = this.hash;
       // using jQuery's animate() method to add smooth page scroll
+      // var offset = 60;
+      var navOffset = 60;
       // the optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - navOffset
       }, 400, function(){
         // add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     }
-
-    // // change text and link with scrolling on click
-    // var text = document.getElementById("nav-link");
-    // // if link text content is "projects"
-    // if (text.textContent === "projects") {
-    //   // change text content to "about me"
-    //   text.textContent = "about me";
-    //   // change a link to "about me"
-    //   $("a").attr("href", "#about");
-    // } else {
-    //   // change text content to "projects"
-    //   text.textContent = "projects";
-    //   // change a link to "projects"
-    //   $("a").attr("href", "#projects");
-    // }
   });
-  
-  // // slick carousel
-  // $('.carouselFrame').slick({
-  //   arrows: true,
-  //   infinite: true,
-  //   centerMode: true,
-  //   centerPadding: 0
-  // }) ;
+
+  var offsetSize = $(".navbar").innerHeight();
+
+  $('html, body').animate({
+    scrollTop:$(window.location.hash).offset().top-offsetSize }, 400);
 });
+
+// // change text and link with scrolling on click
+// var text = document.getElementById("nav-link");
+// // if link text content is "projects"
+// if (text.textContent === "projects") {
+//   // change text content to "about me"
+//   text.textContent = "about me";
+//   // change a link to "about me"
+//   $("a").attr("href", "#about");
+// } else {
+//   // change text content to "projects"
+//   text.textContent = "projects";
+//   // change a link to "projects"
+//   $("a").attr("href", "#projects");
+// }
+// });
+
+// // slick carousel
+// $('.carouselFrame').slick({
+//   arrows: true,
+//   infinite: true,
+//   centerMode: true,
+//   centerPadding: 0
+// }) ;
